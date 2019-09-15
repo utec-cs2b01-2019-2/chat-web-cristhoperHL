0dc4208d110a63d9ad3a33025b0aedb67f99a8be
cristhoperHL <cristhopermma@gmail.com>
2019-09-15 17:35:42 -0500

    agregue mi chat y mi login simple.

--- web/server.py
+++ web/server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, session, Response, redirect
+from flask import Flask,render_template, request, session, Response, redirect, url_for
 from database import connector
 from model import entities
 import datetime
@@ -9,12 +9,6 @@ db = connector.Manager()
 engine = db.createEngine()
 
 app = Flask(__name__)
-
-
-
-@app.route('/cuantasletras/<nombre>')
-def cuantas_letras(nombre):
-	return str(len(nombre))
 
 @app.route('/')
 def index():
@@ -182,29 +176,6 @@ def send_message():
     session.commit()
     return 'Message sent'
 
-@app.route('/authenticate', methods = ['POST'])
-def authenticate():
-    #Get data form request
-    time.sleep(3)
-    message = json.loads(request.data)
-    username = message['username']
-    password = message['password']
-
-    # Look in database
-    db_session = db.getSession(engine)
-
-    try:
-        user = db_session.query(entities.User
-            ).filter(entities.User.username==username
-            ).filter(entities.User.password==password
-            ).one()
-        session['logged_user'] = user.id
-        message = {'message':'Authorized'}
-        return Response(message, status=200,mimetype='application/json')
-    except Exception:
-        message = {'message':'Unauthorized'}
-        return Response(message, status=401,mimetype='application/json')
-
 @app.route('/current', methods = ['GET'])
 def current_user():
     db_session = db.getSession(engine)
@@ -215,6 +186,34 @@ def current_user():
 def logout():
     session.clear()
     return render_template('login.html')
+
+#stateless interaction
+@app.route('/cuantasletras/<nombre>')
+def cuantasletras(nombre):
+    return str(len(nombre))
+
+#statefull interaction
+@app.route('/suma/<numero>')
+def suma(numero):
+    if 'suma' not in session:
+        session['suma'] = 0
+
+    suma = session['suma']
+    suma = suma + int(numero)
+    session['suma'] = suma
+    return str(suma)
+
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'cristhoper14' and password == '10052001':
+        session['usuario'] = username
+        return redirect('http://127.0.0.1:8000/static/chat.html')
+    else:
+        return "Sorry " + username+" you are not a valid user"
+
+
 
 if __name__ == '__main__':
     app.secret_key = ".."
